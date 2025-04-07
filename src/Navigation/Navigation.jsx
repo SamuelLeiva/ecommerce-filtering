52c3d8e5486d73736949a23633b46950e73ffd65
SamuelLeiva <leiva20152@gmail.com>
2025-04-07 10:10:35 -0500

    Added main.css

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,9 +1,28 @@
+import {FiHeart} from 'react-icons/fi'
+import {AiOutlineShoppingCart, AiOutlineUserAdd} from 'react-icons/ai'
+
 import "./Navigation.css"
 import React from "react";
 
 function Navigation() {
   return (
-    <h1>Nav</h1>
+    <nav>
+      <div className="nav-container">
+        <input type="text" className="search-input" placeholder="Enter your search shoes." />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className='nav-icons' />
+        </a>
+
+        <a href="#">
+          <AiOutlineShoppingCart className='nav-icons' />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className='nav-icons' />
+        </a>
+      </div>
+    </nav>
   )
 }
 
