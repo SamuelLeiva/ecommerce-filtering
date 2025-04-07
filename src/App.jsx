9ba2de3d3f4946ec7dffa44956d5d3dd04b58581
SamuelLeiva <leiva20152@gmail.com>
2025-04-07 10:56:50 -0500

    Added Sidebar structure and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Navigation from "./Navigation/Navigation";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
 
 const App = () => {
   return (
     <>
+      <Sidebar />
       <Navigation />
       <Recommended />
       <Products />
