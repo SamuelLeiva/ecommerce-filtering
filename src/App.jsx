e1763ef1a2c4c4640faa848f93b7988b55a56514
SamuelLeiva <leiva20152@gmail.com>
2025-04-08 12:47:09 -0500

    Written functionalities and states in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "./Navigation/Navigation";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
 
+//database
+import products from "./db/data";
+import Card from "./components/Card";
+
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  //----------- Input filter -----------------
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredItems = products.filter((product) =>
+    product.title.toLowerCase().indexOf(query.toLowerCase() !== -1)
+  );
+
+  //----------- radio filter ---------------
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  //----------- Buttons filter ---------------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const filteredData = (products, selected, query) => {
+    let filteredProducts = products;
+
+    //Filtering Input items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Selected Filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, prevPrice, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          prevPrice === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(({ img, title, star, reviews, prevPrice, newPrice }) => (
+      <Card
+        key={Math.random()}
+        img={img}
+        title={title}
+        star={star}
+        reviews={reviews}
+        prevPrice={prevPrice}
+        newPrice={newPrice}
+      />
+    ));
+  };
+
+  const result = filteredData(products, selectedCategory, query)
+
   return (
     <>
       <Sidebar />
